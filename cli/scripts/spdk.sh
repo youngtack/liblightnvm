8982d7af430697b6f3315b66b199e68a6712d9c3
Simon A. F. Lund <slund@cnexlabs.com>
2017-09-29 13:50:38 +0200

    nvm_be_spdk: cli: scripts: Tool for common SPDK test

--- cli/scripts/spdk.sh
+++ cli/scripts/spdk.sh
@@ -11,16 +11,15 @@ do
 		echo "## Building liblightnvm"
 		make debug spdk
 		;;
-	vgrnd)
-		echo "## Running nvm_dev info (with algrind)"
-		NVM_CLI_BE_ID="0x8" sudo valgrind nvm_dev info "traddr:0000:01:00.0"
-		;;
-
 	info)
 		echo "## Running nvm_dev info"
 		NVM_CLI_BE_ID="0x8" sudo nvm_dev info "traddr:0000:01:00.0"
 		#NVM_CLI_BE_ID="0x8" sudo nvm_dev info "traddr:0000:02:00.0"
 		#NVM_CLI_BE_ID="0x8" sudo nvm_dev info FOO
+		;;
+	vgrnd)
+		echo "## Running nvm_dev info (with algrind)"
+		NVM_CLI_BE_ID="0x8" sudo valgrind nvm_dev info "traddr:0000:01:00.0"
 		;;
 	*)
 		break
